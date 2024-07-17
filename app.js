815bee36b21d8e897ef04747471cac6922393592
Andres David <kyfunky1386@gmail.com>
2024-07-17 16:16:26 -0500

    guardado

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const sequelize = require('./utils/database.js');
-const models = require('./models/relations.js');
-
 
 const cors = require('cors'); 
 require('dotenv').config();
@@ -22,7 +20,6 @@ app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     res.send(`<h1>¡Hola usuario!</h1>`);
-    console.log("Bienvenido al servidor de mantenimiento de autos");
 });
 
 // Importar y usar el enrutador para '/user' y '/vehiculos'
@@ -33,17 +30,32 @@ app.use("/mantenimientos", require("./routes/registroMantenimiento.js"));
 // Conectar a la base de datos y arrancar el servidor
 sequelize.authenticate()
     .then(() => {
-        console.log('Conexión establecida correctamente.');
-        // Sincronizar modelos con la base de datos (opcional, crea tablas si no existen)
         return sequelize.sync({ force: false });
     })
     .then(() => {
-        console.log('Modelos sincronizados con la base de datos.');
-        // Iniciar el servidor una vez que todo esté listo
+
         app.listen(port, () => {
             console.log(`El servidor está escuchando en el puerto: ${port}`);
         });
+
+        // Mantener el servidor despierto
+        keepServerAwake();
     })
     .catch(err => {
         console.error('Error al conectar con la base de datos:', err);
     });
+
+// Función para mantener el servidor despierto
+const keepServerAwake = () => {
+    setInterval(() => {
+        const http = require('http');
+        http.get(`http://localhost:${port}`, (res) => {
+            res.on('data', () => {});
+            res.on('end', () => {
+                null
+            });
+        }).on('error', (err) => {
+            console.error('Error en la solicitud para mantener el servidor despierto:', err);
+        });
+    }, 20000); // Cada 5 minutos
+};
